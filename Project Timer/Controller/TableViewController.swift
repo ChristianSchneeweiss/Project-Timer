0584fc9e9b0d663614de60af0c55cc4e9b2cfdfa
Christian Schneeweiss <christianschneeweiss@icloud.com>
2018-06-11 10:49:54 +0200

    app icon change

--- Project Timer/Controller/TableViewController.swift
+++ Project Timer/Controller/TableViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Christian Schneeweiss on 09.06.18.
 //  Copyright © 2018 Christian Schneeweiss. All rights reserved.
-//  Appicon : <div>Icons made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//  Appicon : <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 import UIKit
 import RealmSwift
